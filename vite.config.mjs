1bf9b4d379d21a9985cd55b90625232235ce730a
RGdevz <72873414+RGdevz@users.noreply.github.com>
2023-02-03 19:27:17 +0200

    b

--- vite.config.mjs
+++ vite.config.mjs
@@ -22,7 +22,7 @@ export default defineConfig( {
    // Could also be a dictionary or array of multiple entry points
    entry: resolve(__dirname, 'src/index.ts'),
    name: 'storage_helper',
-   // the proper extensions will be added
+   formats: ['es', 'cjs'],
    fileName: 'storage_helper',
   },
 
