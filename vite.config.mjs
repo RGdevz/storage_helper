e8a9f4caafc87272aa62ae7146df011cbce154e8
RGdevz <72873414+RGdevz@users.noreply.github.com>
2023-10-24 15:50:09 +0300

    asd

--- vite.config.mjs
+++ vite.config.mjs
@@ -4,6 +4,14 @@ import dts from 'vite-plugin-dts';
 import { defineConfig } from 'vite'
 
 import builtinModules from "builtin-modules";
+import packageJson from "./package.json";
+
+const dependencies = Object.keys({
+ ...packageJson.dependencies,
+ ...packageJson.devDependencies,
+}
+);
+
 
 const wtf = builtinModules.map(x=>`node:${x}`)
 
@@ -21,11 +29,11 @@ export default defineConfig( {
      inlineDynamicImports: true,
      // make sure to externalize deps that shouldn't be bundled
      // into your library
-     external: [builtinModules,...wtf],
+     external: [builtinModules,...wtf,...dependencies],
 
     },
 
-
+minify:false,
     lib: {
    // Could also be a dictionary or array of multiple entry points
    entry: resolve(__dirname, 'src/index.ts'),
